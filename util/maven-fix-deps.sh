006da7befe3e51c55ac6d7117d43ad3367525cd4
Philip Panyukov <ppanyukov@googlemail.com>
2011-06-13 23:21:33 +0100

    Update maven fix script with new lib location.
    The libs moved to ~/local/lib rather than being in ~/local.

--- util/maven-fix-deps.sh
+++ util/maven-fix-deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This is a generic maven local repo setup for the dependencies.
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-api-1.0-sdk -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/appengine-java-sdk-1.5.0.1/lib/user/appengine-api-1.0-sdk-1.5.0.1.jar
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-api-labs -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/appengine-java-sdk-1.5.0.1/lib/user/appengine-api-labs-1.5.0.1.jar
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/appengine-java-sdk-1.5.0.1/lib/impl/appengine-local-runtime.jar
-mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/appengine-java-sdk-1.5.0.1/lib/impl/appengine-local-runtime.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-api-1.0-sdk -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/lib/appengine-java-sdk-1.5.0.1/lib/user/appengine-api-1.0-sdk-1.5.0.1.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-api-labs -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/lib/appengine-java-sdk-1.5.0.1/lib/user/appengine-api-labs-1.5.0.1.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/lib/appengine-java-sdk-1.5.0.1/lib/impl/appengine-local-runtime.jar
+mvn install:install-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.5.0.1 -Dpackaging=jar -Dfile=${HOME}/local/lib/appengine-java-sdk-1.5.0.1/lib/impl/appengine-local-runtime.jar
